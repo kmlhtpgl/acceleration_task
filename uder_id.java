a857dd612c99af1e8d75bf0a4fb74f212d574cee
kmlhtpgl <75902689+kmlhtpgl@users.noreply.github.com>
2024-07-07 23:43:05 +0100

    Update uder_id.java

--- uder_id.java
+++ uder_id.java
@@ -1,3 +1,6 @@
+//Add the following JavaScript code to your website. Place it in a location where the User-ID is available, typically after the user logs in or on every page load if the user is always logged in.
+
+
 // Assuming `user_id` is a JavaScript variable containing the logged-in user's ID
 gtag('config', 'G-XXXXXX', {
   'user_id': user_id
